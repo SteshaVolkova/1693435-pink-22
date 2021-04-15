e8e382c6c0992ff793984d5c07f05b57263e2803
stesha <stepashechka5@gmail.com>
2021-04-15 19:07:18 +0300

    Исправила, согласно дополнениям

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,11 @@
 var navMain = document.querySelector('.page-header');
-
+var pageMain = document.querySelector('.page-main');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 
-
 navMain.classList.remove('main-nav--nojs');
-
+navMain.classList.remove('main-nav--opened');
+pageMain.classList.remove('main-nav--nojs');
 
 
 navToggle.addEventListener('click', function() {
